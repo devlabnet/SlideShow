945876d74a228677e5b1350a8bf471d17b12cae9
Pascal Pieper <ppieper@fb3.uni-bremen.de>
2017-08-26 15:01:20 +0200

    Added translation for german language

---
+++ lang/de.js
@@ -0,0 +1,41 @@
+﻿/*
+Copyright (c) 2003-2013, Cricri042. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+Targeted for "ad-gallery" JavaScript : http://adgallery.codeplex.com/
+And "Fancybox" : http://fancyapps.com/fancybox/
+*/
+CKEDITOR.plugins.setLang( 'slideshow', 'de', {
+	toolbar	: 'SlideShow',
+	dialogTitle : 'SlideShow Eigenschaften',
+	fakeObject : 'SlideShow',
+	imgList : 'Bilder',
+	imgAdd : 'Bilder hinzufügen',
+	imgTitle : 'Titel',
+	imgDelete : 'Löschen',
+	imgEdit : 'Bearbeiten',
+    validModif : 'Speichern',
+	previewMode : 'Vorschau',
+	imgDesc : 'Kurzbeschreibung',
+	editSlideShow : 'Slideshow bearbeiten',
+	picturesList : "Liste der Bilder :",
+	insertSlideShow : 'SlideShow einfügen',
+	showThumbs : 'Vorschaubilder anzeigen',
+	showTitle : 'Titel anzeigen',
+	showControls : "Start / Stop Steuerung anbieten",
+	labelStart : "Start",
+	labelStop : "Stop",
+	arrowUp : "\u2191",
+	arrowDown : "\u2193",
+	displayTime : 'Anzeigedauer (Sec.)',
+	transitionTime : 'Übergangsdauer (mSec.)',
+	autoStart : 'Autostart',
+	pictHeight : 'Höhe (px)',
+	pictWidth : 'Breite (px)',
+	openOnClick : 'Per Klick öffnen',
+	transition : 'Übergangstyp',
+	tr1 : 'Keine',
+	tr2 : 'Größe verändern',
+	tr3 : 'Vertikal verschieben',
+	tr4 : 'Horizontal verschieben',
+	tr5 : 'Ausblenden',
+});
